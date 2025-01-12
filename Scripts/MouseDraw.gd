689939240469fc5b1d9e9e0beb8ccf8584f464b6
Knower Coder <msivabalanchennai@gmail.com>
2025-01-12 12:07:20 +0530

    updated

---
+++ Scripts/MouseDraw.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var bufferRectMaterial : ShaderMaterial
+
+var prevMousePos = Vector2(-1000, 0)
+var mousePos = Vector2(-1000, 0)
+
+func _ready():	
+	bufferRectMaterial = $"../BufferViewport/ColorRect".material	
+	
+func _physics_process(delta):	
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):		
+		mousePos = get_viewport().get_mouse_position()
+		bufferRectMaterial.set_shader_parameter("_mousePos", mousePos)
+		bufferRectMaterial.set_shader_parameter("_prevMousePos", prevMousePos)		
+		prevMousePos = mousePos
+	else:
+		prevMousePos = Vector2(-1000, 0)
+	
+	
+	
